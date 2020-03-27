21fc980b9946296ed0acf02132de262e9c08ead8
Enrico <Enrico@192.168.1.14>
2020-03-27 12:03:23 +0100

    Aldo impara a formattare!!! Pirla

--- tama-base/src/it/unibs/ing/fp/tamaBase/TamaBase.java
+++ tama-base/src/it/unibs/ing/fp/tamaBase/TamaBase.java
@@ -1,55 +1,63 @@
 package it.unibs.ing.fp.tamaBase;
 
 public class TamaBase {
-	
+
 	public static final int MINIMO_VALORI = 0;
 	public static final int MASSIMO_VALORI = 100;
-	//public static final int N_TAMAGOTCHI = 1;
+	// public static final int N_TAMAGOTCHI = 1;
 	public static final int VAL_FELICITA = 80;
 	public static final int VAL_INFELICITA = 30;
-	
+
 	public int soddisfazione;
 	public int sazieta;
 	public String nome;
-	
+
 	public String toString() {
+		
 		StringBuffer messaggio = new StringBuffer();
-		messaggio.append(String.format("Stato attuale :\nNome = %s\nSazieta = %d\nSoddisfazione = %d\n",nome,sazieta,soddisfazione));
-		if(verificaFelicita() || verificaInfelicita()){
+		
+		messaggio.append(String.format("Stato attuale :"
+				+ "Nome = %s"
+				+ "Sazieta = %d"
+				+ "Soddisfazione = %d"
+				, nome, sazieta, soddisfazione));
+		
+		if (verificaFelicita() || verificaInfelicita()) {
 			messaggio.append("Il tamagotchi e' ");
 			messaggio.append(verificaFelicita() ? "felice\n" : "");
 			messaggio.append(verificaInfelicita() ? "triste\n" : "");
 		}
-		
+
 		return messaggio.toString();
-		
+
 	}
-	
-	public void daiCarezza(int quante){
-		
-		for(int i = 0; i<quante; i++) {
-			if(soddisfazione <= MASSIMO_VALORI)
-				soddisfazione+=1;
-			if(soddisfazione>MASSIMO_VALORI)
-				soddisfazione=MASSIMO_VALORI;
-			
+
+	public void daiCarezza(int quante) {
+
+		for (int i = 0; i < quante; i++) {
+			if (soddisfazione <= MASSIMO_VALORI)
+				soddisfazione += 1;
+			if (soddisfazione > MASSIMO_VALORI)
+				soddisfazione = MASSIMO_VALORI;
+
 		}
-		sazieta-=5;
-		if(sazieta<0)
-			sazieta=0;
+		sazieta -= 5;
+		if (sazieta < 0)
+			sazieta = 0;
 	}
-	public void daiBiscotto(int quante){
-		
-		for(int i = 0; i<quante; i++) {
-			if(sazieta < MASSIMO_VALORI)
-				sazieta+=1;
-			if(sazieta>MASSIMO_VALORI)
-				sazieta=MASSIMO_VALORI;
+
+	public void daiBiscotto(int quante) {
+
+		for (int i = 0; i < quante; i++) {
+			if (sazieta < MASSIMO_VALORI)
+				sazieta += 1;
+			if (sazieta > MASSIMO_VALORI)
+				sazieta = MASSIMO_VALORI;
 		}
-		
-		soddisfazione-=5;
-		if(soddisfazione<0)
-			soddisfazione=0;
+
+		soddisfazione -= 5;
+		if (soddisfazione < 0)
+			soddisfazione = 0;
 	}
 
 	public boolean verificaFelicita() {
--- tama-base/src/it/unibs/ing/fp/tamaBase/TamaMain.java
+++ tama-base/src/it/unibs/ing/fp/tamaBase/TamaMain.java
@@ -8,32 +8,35 @@ public class TamaMain {
 	public static final int MAX_BISCOTTI = 20;
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Tamagotchi (test v1.0)");
-		
+
 		TamaBase tama1 = TamaUtils.creaTama();
-		
-		//System.out.println(tama1.toString());
+
+		// System.out.println(tama1.toString());
 
 		int scelta;
-		
+
 		do {
-			
-			scelta = InputDati.leggiIntero("Cosa vuoi fare?\n1 per dare una carezza\n2 per dare un biscotto\n0 per uscire: ", 0 , 2); 
-			
+
+			scelta = InputDati.leggiIntero(
+					"Cosa vuoi fare?\n1 per dare una carezza\n2 per dare un biscotto\n0 per uscire: ", 0, 2);
+
 			switch (scelta) {
-				case 1:
-					int n_carezze = InputDati.leggiIntero(String.format("Quante carezze vuoi dargli (da 1 a %d) = ",MAX_CAREZZE), 1, MAX_CAREZZE);
-					tama1.daiCarezza(n_carezze);
+			case 1:
+				int n_carezze = InputDati.leggiIntero(
+						String.format("Quante carezze vuoi dargli (da 1 a %d) = ", MAX_CAREZZE), 1, MAX_CAREZZE);
+				tama1.daiCarezza(n_carezze);
 				break;
-				case 2:
-					int n_biscotti = InputDati.leggiIntero(String.format(("Quanti biscotti vuoi dargli (da 1 a %d) = "),MAX_BISCOTTI), 1, MAX_BISCOTTI);
-					tama1.daiBiscotto(n_biscotti);
+			case 2:
+				int n_biscotti = InputDati.leggiIntero(
+						String.format(("Quanti biscotti vuoi dargli (da 1 a %d) = "), MAX_BISCOTTI), 1, MAX_BISCOTTI);
+				tama1.daiBiscotto(n_biscotti);
 				break;
 			}
-			
+
 			System.out.print(tama1.toString());
-		}while(scelta != 0);
+		} while (scelta != 0);
 	}
 
 }
